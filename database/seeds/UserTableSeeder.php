1f5af79d867d4d0baa78373028335c38e0afaa9f
Christopher Lamm <chris@theantichris.com>
2015-07-19 09:25:17 -0400

    User seeder.

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $testUser = new \TheRogg\User();
+        $testUser->name = 'Christopher';
+        $testUser->save();
+    }
+}
