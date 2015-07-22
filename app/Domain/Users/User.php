1f3e3cbaef34b365baa9cfb86248953d3479bc8d
Christopher Lamm <chris@theantichris.com>
2015-07-21 20:41:39 -0400

    User extending Entity.

--- app/Domain/Users/User.php
+++ app/Domain/Users/User.php
@@ -6,9 +6,8 @@ use Illuminate\Auth\Authenticatable;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
-use Jenssegers\Mongodb\Model;
 
-class User extends Model implements AuthenticatableContract, CanResetPasswordContract
+class User extends Entity implements AuthenticatableContract, CanResetPasswordContract
 {
     use Authenticatable, CanResetPassword;
 
@@ -16,12 +15,15 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 
     protected $hidden = ['password', 'remember_token'];
 
-    public function setEmail($email) { $this->attributes['email'] = $email; }
     public function getEmail() { return $this->attributes['email']; }
+    public function setEmail($email) { $this->attributes['email'] = $email; }
 
-    public function setName($name) { $this->attributes['name'] = $name; }
     public function getName() { return $this->attributes['name']; }
+    public function setName($name) { $this->attributes['name'] = $name; }
 
-    public function setPassword($password) { $this->attributes['password'] = $password; }
     public function getPassword() { return $this->attributes['password']; }
+    public function setPassword($password)
+    {
+        $this->attributes['password'] = $password;
+    }
 }
