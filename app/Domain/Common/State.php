1ce04f4f913335e3129a08c2fa5a6abf1a43a024
Christopher Lamm <chris@theantichris.com>
2015-07-21 21:38:18 -0400

    State enum.

---
+++ app/Domain/Common/State.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace TheRogg\Domain;
+
+abstract class State
+{
+    const Alabama            = 1;
+    const Alaska             = 2;
+    const Arizona            = 3;
+    const Arkansas           = 4;
+    const California         = 5;
+    const Colorado           = 6;
+    const Connecticut        = 7;
+    const Delaware           = 8;
+    const DistrictOfColumbia = 9;
+    const Florida            = 10;
+    const Georgia            = 11;
+    const Hawaii             = 12;
+    const Idaho              = 13;
+    const Illinois           = 14;
+    const Indiana            = 15;
+    const Iowa               = 16;
+    const Kansas             = 17;
+    const Kentucky           = 18;
+    const Louisiana          = 19;
+    const Maine              = 20;
+    const Maryland           = 21;
+    const Massachusetts      = 22;
+    const Michigan           = 23;
+    const Minnesota          = 24;
+    const Mississippi        = 25;
+    const Missouri           = 26;
+    const Montana            = 27;
+    const Nebraska           = 28;
+    const Nevada             = 29;
+    const NewHampshire       = 30;
+    const NewJersey          = 31;
+    const NewMexico          = 32;
+    const NewYork            = 33;
+    const NorthCarolina      = 34;
+    const NorthDakota        = 35;
+    const Ohio               = 36;
+    const Oklahoma           = 37;
+    const Oregon             = 38;
+    const Pennsylvania       = 39;
+    const RhodeIsland        = 40;
+    const SouthCarolina      = 41;
+    const SouthDakota        = 42;
+    const Tennessee          = 43;
+    const Texas              = 44;
+    const Utah               = 45;
+    const Vermont            = 46;
+    const Virginia           = 47;
+    const Washington         = 48;
+    const WestVirginia       = 49;
+    const Wisconsin          = 50;
+    const Wyoming            = 51;
+}
