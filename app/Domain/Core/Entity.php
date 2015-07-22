38249803bfb8b1d0ba0073bae9e64f1d066ec31e
Christopher Lamm <chris@theantichris.com>
2015-07-21 21:01:11 -0400

    Added ID setter.

--- app/Domain/Core/Entity.php
+++ app/Domain/Core/Entity.php
@@ -2,11 +2,19 @@
 
 namespace TheRogg\Domain;
 
+use InvalidArgumentException;
 use Jenssegers\Mongodb\Model;
 
 abstract class Entity extends Model implements EntityInterface
 {
     public function getId() { $this->attributes['_id']; }
+    public function setId($id)
+    {
+        if ($this->stringIsNullOrEmpty($id))
+            throw new InvalidArgumentException('ID cannot be empty.');
+
+        $this->attributes['_id'] = $id;
+    }
 
     protected function stringIsNullOrEmpty($string)
     {
