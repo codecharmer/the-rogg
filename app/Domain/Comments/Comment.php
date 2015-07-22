403712dcbef7707af7745c24ba1cbee487ff70bc
Christopher Lamm <chris@theantichris.com>
2015-07-21 21:10:16 -0400

    Comment model.

---
+++ app/Domain/Comments/Comment.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace TheRogg\Domain;
+
+use InvalidArgumentException;
+
+class Comment extends Entity
+{
+    /** @var  string */
+    protected $userId;
+    /** @var  string */
+    protected $text;
+
+    protected $attributes = [
+        'text' => '',
+    ];
+
+    public function getUserId() { return $this->attributes['userId']; }
+    public function setUserId($userId)
+    {
+        if ($this->stringIsNullOrEmpty($userId))
+            throw new InvalidArgumentException('User ID cannot be empty.');
+
+        $this->attributes['userId'] = $userId;
+    }
+
+    public function getText() { return $this->attributes['text']; }
+    public function setText($text)
+    {
+        if ($this->stringIsNullOrEmpty($text))
+            throw new InvalidArgumentException('Text cannot be empty.');
+
+        $this->attributes['text'] = $text;
+    }
+}
