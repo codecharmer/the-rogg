fd26450c3519136ed591628e1b4a279d2bedeb9e
Christopher Lamm <chris@theantichris.com>
2015-07-21 21:23:03 -0400

    House and Party fake enums.

---
+++ app/Domain/Common/House.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TheRogg\Domain;
+
+
+abstract class House
+{
+    const SENATE          = 1;
+    const REPRESENTATIVES = 2;
+}
---
+++ app/Domain/Common/Party.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TheRogg\Domain;
+
+abstract class Party
+{
+    const DEMOCRAT    = 1;
+    const REPUBLICAN  = 2;
+    const INDEPENDENT = 3;
+}
