b09ea8f508ffbe5c0c1a9f5763632223a3cbeff6
Christopher Lamm <chris@theantichris.com>
2015-07-24 20:03:52 -0400

    Controller stub.

---
+++ app/Http/Controllers/Users/UserControllers.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TheRogg\Http\Controllers\Users;
+
+use TheRogg\Http\Controllers\Controller;
+
+class UserControllers extends Controller
+{
+    public function getGetList()
+    {
+        // TODO: Implement.
+    }
+
+    public function getGetUser()
+    {
+        // TODO: Implement.
+    }
+}
