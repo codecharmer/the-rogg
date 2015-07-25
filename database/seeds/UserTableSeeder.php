1492d9f6e57a61712d7c7bc404410e8a2f526ab9
Christopher Lamm <chris@theantichris.com>
2015-07-24 20:03:59 -0400

    Updates to seeder.

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,22 +1,20 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 use TheRogg\Domain\Party;
-use TheRogg\Domain\User;
+use TheRogg\Repositories\Users\EloquentUserRepository;
 
 class UserTableSeeder extends Seeder
 {
     public function run()
     {
+        /** @noinspection PhpUndefinedMethodInspection */
         Schema::drop('users');
 
-        $user = new User('Christopher Lamm', 'chris@theantichris.com', Hash::make('password'));
-        $user->setParty(Party::Independent);
-        $user->save();
+        $repo = new EloquentUserRepository();
 
-        $user = new User('Suzanne Young', 'suzanne@veil22.com', Hash::make('password'));
-        $user->save();
+        $repo->make('Christopher Lamm', 'chris@theantichris.com', 'password', Party::Independent);
+        $repo->make('Suzanne Young', 'suzanne@veil22.com', 'password');
     }
 }
