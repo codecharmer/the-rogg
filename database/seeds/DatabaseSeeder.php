c34ed078673e806d9c52afb9fa78f889ceeabf06
Christopher Lamm <chris@theantichris.com>
2015-08-04 20:41:32 -0400

    Removed unused seeders.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,9 +6,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        //$this->call(PoliticianTableSeeder::class);
-        //$this->call(UserTableSeeder::class);
-
         $this->call(TestDataSeeder::class);
     }
 }
