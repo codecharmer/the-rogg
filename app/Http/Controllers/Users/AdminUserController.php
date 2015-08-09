afbf3e963f20855a38a9276fc4d3dc73e97eb3d6
Christopher Lamm <chris@theantichris.com>
2015-08-09 17:19:24 -0400

    Adding user.

--- app/Http/Controllers/Users/AdminUserController.php
+++ app/Http/Controllers/Users/AdminUserController.php
@@ -2,13 +2,13 @@
 
 namespace TheRogg\Http\Controllers\Users;
 
+use Exception;
 use Request;
 use Response;
 use TheRogg\Domain\User;
 use TheRogg\Http\Controllers\Controller;
 use TheRogg\Http\Controllers\Users\Models\AdminUserDetailsModel;
 use TheRogg\Http\Controllers\Users\Models\AdminUserListModel;
-use TheRogg\Http\Controllers\Users\Models\UserListModel;
 use TheRogg\Repositories\Users\UserRepositoryInterface as UserRepo;
 
 class AdminUserController extends Controller
@@ -57,5 +57,36 @@ class AdminUserController extends Controller
         );
 
         return Response::json($model);
+    }
+
+    public function postAddUser()
+    {
+        $model    = Request::json();
+        $username = $model->get('username');
+        $email    = $model->get('email');
+        $password = $model->get('password');
+        $party    = $model->get('party') ? $model->get('party') : null;
+        $isAdmin  = $model->get('isAdmin');
+
+        $user = $this->userRepo->findBy('username', $username);
+        if (!empty($user))
+            throw new Exception($username . ' already exists.');
+
+        try
+        {
+            $user = $this->userRepo->make($username, $email, $password, $party);
+            if ($isAdmin)
+            {
+                $user->makeAdmin();
+                $this->userRepo->save($user);
+            }
+
+            return Response::json($user->getId());
+        }
+        catch (Exception $e)
+        {
+            $this->userRepo->delete($user->getId());
+            throw $e;
+        }
     }
 }
