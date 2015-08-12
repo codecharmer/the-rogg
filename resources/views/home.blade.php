995f521570ab985f6eda7a175ab649e574eb3a56
Christopher Lamm <chris@theantichris.com>
2015-08-11 21:09:39 -0400

    Master layout.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale-1.0">
-    <title>The Rogg</title>
-    <link rel="stylesheet" href="{{ elixir('assets/css/app.css') }}">
-</head>
-<body>
+@extends('layouts.master')
 
-</body>
-</html>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale-1.0">
+    <title>The Rogg @yield('title')</title>
+    <link rel="stylesheet" href="{{ elixir('assets/css/app.css') }}">
+</head>
+<body>
+    <div class="container">
+
+    </div>
+</body>
+</html>
