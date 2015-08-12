67aaad1bc44183f9e50c4bf42b29b3d975527acf
Christopher Lamm <chris@theantichris.com>
2015-08-11 20:27:33 -0400

    Setting up Gulp for CSS and JavaScript. Removed default welcome view.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,10 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('app.scss', 'public/assets/css');
+});
+
+// Outputs to build.
+elixir(function(mix){
+    mix.version('assets/css/app.css');
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale-1.0">
     <title>The Rogg</title>
+    <link rel="stylesheet" href="{{ elixir('assets/css/app.css') }}">
 </head>
 <body>
 
