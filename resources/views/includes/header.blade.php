e737136e003a48b5adbae0e51b56014d6243bd3b
Christopher Lamm <chris@theantichris.com>
2015-08-13 20:59:33 -0400

    Moved header to an include.

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,17 @@
+<header class="Header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="assets/images/logo.png">
+            </div>
+            <div class="col-md-6">
+                <form class="form-horizontal">
+                    <input class="form-control" type="text" placeholder="Search">
+                </form>
+            </div>
+            <div class="col-md-2">
+                <a class="btn btn-default" href="#" role="button">My Login</a>
+            </div>
+        </div>
+    </div>
+</header>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,23 +8,7 @@
 </head>
 <body>
 
-<header class="Header">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <img src="assets/images/logo.png">
-            </div>
-            <div class="col-md-6">
-                <form class="form-horizontal">
-                    <input class="form-control" type="text" placeholder="Search">
-                </form>
-            </div>
-            <div class="col-md-2">
-                <a class="btn btn-default" href="#" role="button">My Login</a>
-            </div>
-        </div>
-    </div>
-</header>
+@include('includes.header')
 
 <div class="Navigation">
     <div class="container">
