e2d4786fb7a9a8863436bf88a34ae7433cbec8ae
Christopher Lamm <chris@theantichris.com>
2015-08-13 20:41:53 -0400

    Header and some of the navigation block.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,8 +7,38 @@
     <link rel="stylesheet" href="{{ elixir('assets/css/app.css') }}">
 </head>
 <body>
-    <div class="container">
 
+<header class="Header">
+    <div class="container">
+        <div class="row Header-row">
+            <div class="col-md-4">
+                <img src="assets/images/logo.png">
+            </div>
+            <div class="col-md-6">
+                <form class="form-horizontal">
+                    <input class="form-control" type="text" placeholder="Search">
+                </form>
+            </div>
+            <div class="col-md-2">
+                <a class="btn btn-default" href="#" role="button">My Login</a>
+            </div>
+        </div>
     </div>
+</header>
+
+<div class="Navigation">
+    <div class="container">
+        <div class="row Navigation-row">
+            <div class="col-md-3">
+                <a class="Navigation-registerButton" href="#" role="button">Create Your Free Account</a>
+            </div>
+            <div class="col-md-9">
+                <p>A resource where your void can truly be heard. Rate, share, and comment on any government official.
+                    Raw and unedited - That's the ROGG!</p>
+            </div>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
