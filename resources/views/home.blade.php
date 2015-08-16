76875d6c58ce695f174ebeed3d7d2f0cfe6d3318
Christopher Lamm <chris@theantichris.com>
2015-08-16 16:17:19 -0400

    Displaying bells.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,15 @@
                             wrote a review for <a href="#">{{ $review->politician->name }}</a></span>
                         </div>
                         <div class="Review--rating">
-                            {{ $review->rating }} on @date($review->timestamp)
+                            <span class="Review--bells">
+                            @for($i = 0; $i < $review->rating; $i++)
+                                    <img class="Review--bell" src="assets/images/liberty-bell.png">
+                                @endfor
+                                @for($i = 0; $i < 5 - $review->rating; $i++)
+                                    <img class="Review--bell" src="assets/images/liberty-bell-dark.png">
+                                @endfor
+                            </span>
+                            <span class="Review--date"> on @date($review->timestamp)</span>
                         </div>
                         <div class="Review--comment">{{ $review->comment }}</div>
                     </div>
