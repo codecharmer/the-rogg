1705ca3ca613bc20482bd3c891de5171db5a72fb
Christopher Lamm <chris@theantichris.com>
2015-08-16 19:22:58 -0400

    Tooltip for rating.

--- resources/views/components/reviewMain.blade.php
+++ resources/views/components/reviewMain.blade.php
@@ -5,18 +5,18 @@
     </div>
     <div class="col-md-10">
         <div class="Review--names">
-                        <span><a href="#">{{ $review->user->username }}</a>
-                            wrote a review for <a href="#">{{ $review->politician->name }}</a></span>
+            <span><a href="#">{{ $review->user->username }}</a> wrote a review for <a href="#">{{ $review->politician->name }}</a></span>
         </div>
         <div class="Review--rating">
-                            <span class="Review--bells">
-                            @for($i = 0; $i < $review->rating; $i++)
-                                    <img class="Review--bell" src="assets/images/liberty-bell.png">
-                                @endfor
-                                @for($i = 0; $i < 5 - $review->rating; $i++)
-                                    <img class="Review--bell" src="assets/images/liberty-bell-dark.png">
-                                @endfor
-                            </span>
+            <span class="Review--bells">
+                @for($i = 0; $i < $review->rating; $i++)
+                    <img class="Review--bell" src="assets/images/liberty-bell.png" alt="{{ $review->rating }}">
+                @endfor
+
+                @for($i = 0; $i < 5 - $review->rating; $i++)
+                    <img class="Review--bell" src="assets/images/liberty-bell-dark.png" alt="{{ $review->rating }}">
+                @endfor
+            </span>
             <span class="Review--date"> {{ $review->timestamp->diffForHumans() }}</span>
         </div>
         <div class="Review--comment">{{ $review->comment }}</div>
