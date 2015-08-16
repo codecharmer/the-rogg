df83be72cb2119b43f708ce8a020fe63615f658d
Christopher Lamm <chris@theantichris.com>
2015-08-16 15:55:27 -0400

    Cleaned up view.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,29 @@
-<?php
-
-/** @var TheRogg\Http\ViewComposers\Home\Models\HomePoliticianReviewModel[] $reviews */
-
-?>
-
 @extends('layouts.master')
 
 @section('main')
     <h2>Your Review Awaits</h2>
     <p>Here are some suggestions to get you started.</p>
     <ul class="ReviewList">
-        <?php foreach($reviews as $review) : ?>
-        <li class="Review">
-            <div class="row">
-                <div class="col-md-2">
-                    <a href="#"><img class="ProfilePhoto" src="assets/images/user-photos/<?= $review->user->photo ?>"></a>
-                </div>
-                <div class="col-md-10">
-                    <div class="Review--names">
-                        <span><a href="#"><?= $review->user->username; ?></a>
-                            wrote a review for <a href="#"><?= $review->politician->name; ?></a></span>
+        @foreach($reviews as $review)
+            <li>
+                <div class="row Review">
+                    <div class="col-md-2">
+                        <a href="#"><img class="ProfilePhoto"
+                                         src="assets/images/user-photos/{{ $review->user->photo }}"></a>
                     </div>
-                    <div class="Review--rating">
-                        <?= $review->rating; ?> on <?= $review->timestamp; ?>
+                    <div class="col-md-10">
+                        <div class="Review--names">
+                        <span><a href="#">{{ $review->user->username }}</a>
+                            wrote a review for <a href="#">{{ $review->politician->name }}</a></span>
+                        </div>
+                        <div class="Review--rating">
+                            {{ $review->rating }} on {{ $review->timestamp }}
+                        </div>
+                        <div class="Review--comment">{{ $review->comment }}</div>
                     </div>
-                    <div class="Review--comment"><?= $review->comment; ?></div>
                 </div>
-            </div>
-        </li>
-        <?php endforeach; ?>
+            </li>
+        @endforeach
     </ul>
 @endsection
 
