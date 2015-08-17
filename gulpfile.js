220f7b6b968c6ca0fd4f9681edfe1cc673c45b91
Christopher Lamm <chris@theantichris.com>
2015-08-16 22:18:30 -0400

    Organized Gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,12 @@
 var elixir = require('laravel-elixir');
 
 elixir(function (mix) {
-    mix.scripts([
+    mix
+        .sass('app.scss', 'public/assets/css')
+        .scripts([
             '../../../node_modules/angular/angular.js',
             'politician-list/app.module.js',
             'politician-list/directives/politician-list.js'
-        ],
-        'public/assets/js/all.js');
-});
-
-elixir(function (mix) {
-    mix.sass('app.scss', 'public/assets/css');
-});
-
-elixir(function (mix) {
-    mix.version(['assets/css/app.css', 'assets/js/all.js']);
+        ], 'public/assets/js/all.js')
+        .version(['assets/css/app.css', 'assets/js/all.js']);
 });
