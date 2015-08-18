9b283fd343eb06f1aa518b784b296b30488c5fce
Christopher Lamm <chris@theantichris.com>
2015-08-17 23:07:14 -0400

    Working on deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,8 +5,10 @@ set :branch, :master
 
 set :deploy_to, "/usr/share/nginx/html/#{fetch(:application)}"
 
-set :log_level, :debug
+set :log_level, :info
 set :pty, true
+
+set :gulp_flags, '--production'
 
 set :linked_files, %w{.env}
 
@@ -17,6 +19,14 @@ namespace :deploy do
       sudo :service, "php5-fpm restart"
     end
   end
+  task :permissions do
+      on roles(:app), in: :sequence, wait: 5 do
+         sudo :chmod, "-R o+w /usr/share/nginx/html/the-rogg.theantichris.com/current/storage/"
+         sudo :chmod, "-R o+w /usr/share/nginx/html/the-rogg.theantichris.com/current/bootstrap/cache/"
+      end
+  end
 end
 
 after 'deploy:publishing', 'deploy:restart'
+after 'deploy:restart', 'deploy:permissions'
+after 'deploy:permissions', 'gulp'
