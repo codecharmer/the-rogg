ee9c86cb77e252e9070ea341856c3b687e5b6f8d
Christopher Lamm <chris@theantichris.com>
2015-08-19 21:48:39 -0400

    Setting up detail.

--- resources/views/politician-detail.blade.php
+++ resources/views/politician-detail.blade.php
@@ -7,7 +7,15 @@
 @section('content')
     <div class="row">
         <div class="col-md-8 Main">
-
+            <div class="row PoliticianDetail">
+                <div class="col-md-3">
+                    <img src="/assets/images/politician-photos/{{ $politicianDetail->politician->photo }}">
+                </div>
+                <div class="col-md-9">
+                    <div><h2 class="PoliticianDetail--name">{{ $politicianDetail->politician->name }}</h2></div>
+                    <div>{{ $politicianDetail->politician->bio }}</div>
+                </div>
+            </div>
         </div>
         <div class="col-md-4 Sidebar">
             @include('components.sidebar')
