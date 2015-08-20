9124137c5f087c7414a005cb821d4960943c5446
Christopher Lamm <chris@theantichris.com>
2015-08-19 23:07:19 -0400

    Policies page.

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -4,8 +4,37 @@
     - Policies and Terms of Service
 @endsection
 
-@section('main')
+@section('content')
     <h2>Policies</h2>
 
-    <h2>Terms of Service</h2>
+    <h3>Comment Rules</h3>
+
+    <ul>
+        <li>You will stay on topic.</li>
+        <li>You will not spam. (Spam is flooding the Internet with unnecessary or out of topic comments)</li>
+        <li>You will not include links to websites and videos not associated with the topic.</li>
+        <li>You will not post the same comment multiple times on the same of different articles</li>
+        <li>You will not solicit anyone to buy or sell products or services, or to make donations of any kind. You will not include links to products in your status updates, comments, articles or groups.</li>
+        <li>You will not post anything libelous, defamatory, harmful, threatening, harassing, abusive, invasive of another's privacy, hateful, racially or ethnically objectionable, or otherwise illegal.</li>
+        <li>You will not make threats to other users or people not associated with the site.</li>
+        <li>If you violate these rules, your comment(s) and/or user name will be deleted.</li>
+    </ul>
+
+    <h3>Digital Millennium Copyright Act</h3>
+
+    <p>
+        It is our policy to respond to notices of alleged infringement that comply with the Digital Millennium Copyright Act (found at the U.S. Copyright Office) and other applicable intellectual property laws. It is our policy to remove material from public view that we believe in good faith to be copyrighted material that has been illegally copied and distributed by any of our members or users. It is also our policy to terminate the accounts of repeat offenders. If we remove or disable access to comply with the Digital Millennium Copyright Act we will make a good-faith attempt to contact the owner, author, or administrator of each affected site so that they may make a counter notification pursuant to sections 512(g)(2) and (3) of that Act.
+    </p>
+
+    <h3>Infringement Notification</h3>
+
+    <p>
+        If you believe that content residing on or accessible through our website infringes a copyright for which you own or are a designated agent, please send a notice of infringement by contacting us at dcma@therogg.com with information that sets forth the items specified below.
+    </p>
+
+    <p>To expedite our ability to process your request, please use the following format:</p>
+
+    <p>Title of the specific thread or article that contains the copyrighted work. Post number of the specific post that contains the copyrighted work. Link to the specific post (available via the "this post" link on each post) or article you believe contains the copyrighted material. Specific nature of the copyright infringement you believe is occurring. Details of your claim to the material, or your relationship to the material copyright holder. Provide your full name, address, and telephone number should we need to clarify your claim. Provide a working email address where we can contact you to confirm your claim. Include the following statement: "I have a good faith belief that use of the copyrighted materials described above on the allegedly infringing web pages is not authorized by the copyright owner, its agent, or the law." Include the following statement: "I swear, under penalty of perjury, that the information in the notification is accurate and that I am the copyright owner or am authorized to act on behalf of the copyright owner to make this complaint."</p>
+
+    <p>We will review your claim and determine its merit. If, after our review, we believe your claim is sufficient to remove the information from public view, we will do so immediately and provide you with confirmation. If your claim is insufficient to act upon, or, in our consideration the material has been posted with a good faith effort and compliant with the DMCA, we will notify you and provide you opportunity to clarify or seek additional remedy.</p>
 @endsection
