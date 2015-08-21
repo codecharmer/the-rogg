c1a6bbcd3c455f45c57aed5c1d3714db9fc940d4
Christopher Lamm <chris@theantichris.com>
2015-08-21 00:59:52 -0400

    Politician images and seeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,6 +6,7 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $this->call(TestDataSeeder::class);
+        //$this->call(TestDataSeeder::class);
+        $this->call(PoliticianSeeder::class);
     }
 }
