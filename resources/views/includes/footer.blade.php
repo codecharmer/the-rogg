a01f2694be1cd79951dab414a6174c7a9fe3d924
Christopher Lamm <chris@theantichris.com>
2015-08-21 00:03:36 -0400

    No footer content for right now.

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,18 +1,22 @@
 <footer class="Footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3">
-                <h4 class="Footer-sectionHeader">About Rogg</h4>
-            </div>
-            <div class="col-md-3">
-                <h4 class="Footer-sectionHeader">Resources</h4>
-            </div>
-            <div class="col-md-3">
-                <h4 class="Footer-sectionHeader">My Login</h4>
-            </div>
-            <div class="col-md-3">
-                <h4 class="Footer-sectionHeader">Follow Rogg</h4>
-            </div>
-        </div>
-    </div>
+    {{--<div class="container">--}}
+        {{--<div class="row">--}}
+            {{--<div class="col-md-3">--}}
+                {{--<h4 class="Footer-sectionHeader">About Rogg</h4>--}}
+            {{--</div>--}}
+            {{--<div class="col-md-3">--}}
+                {{--<h4 class="Footer-sectionHeader">Resources</h4>--}}
+            {{--</div>--}}
+            {{--<div class="col-md-3">--}}
+                {{--<h4 class="Footer-sectionHeader">My Login</h4>--}}
+            {{--</div>--}}
+            {{--<div class="col-md-3">--}}
+                {{--<h4 class="Footer-sectionHeader">Follow Rogg</h4>--}}
+                {{--<i class="fa fa-facebook-official"></i>--}}
+                {{--<i class="fa fa-google-plus-square"></i>--}}
+                {{--<i class="fa fa-twitter-square"></i>--}}
+                {{--<i class="fa fa-linkedin-square"></i>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 </footer>
