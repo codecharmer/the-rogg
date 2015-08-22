6e6b9d0ccf37652d1acb69a8a59f15927dc034cf
Christopher Lamm <chris@theantichris.com>
2015-08-21 21:14:44 -0400

    Centering buttons in header.

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,21 +1,17 @@
 <header class="Header">
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
-                <img class="Header--logo" src="/assets/images/logo.png">
+            <div class="col-md-offset-2 col-md-8 text-center">
+                <a title="Home" href="/"><img class="Header--logo" src="/assets/images/logo.png"></a>
             </div>
-            <div class="col-md-6">
-                {{--<form class="form-horizontal">--}}
-                {{--<input class="form-control" type="text" placeholder="Search">--}}
-                {{--</form>--}}
-            </div>
-            <div class="col-md-2">
+            <div class="col-md-2 Header--buttonContainer text-center">
                 @if(Auth::guest())
-                    <a class="btn btn-default" href="/auth/login" role="button">Login</a>
+                    <a title="Login" class="btn btn-default" href="/auth/login" role="button">Login</a>
+                    <a title="Register" class="Header--link btn btn-link" href="/auth/register">Register</a>
                 @endif
 
                 @if(Auth::check() || Auth::viaRemember())
-                    <a class="btn btn-default" href="/auth/logout" role="button">Logout</a>
+                    <a title="Logout" class="btn btn-default" href="/auth/logout" role="button">Logout</a>
                 @endif
             </div>
         </div>
