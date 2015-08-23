9d130b2dac1885a90fa6c9f69be69eada5080ab5
Christopher Lamm <chris@theantichris.com>
2015-08-23 08:29:40 -0400

    Footer.

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,12 +3,40 @@
         <div class="row">
             <div class="col-md-3">
                 <h4 class="Footer-sectionHeader">About The Rogg</h4>
+                <a href="/mission" title="The Rogg's Mission">Mission</a>
             </div>
             <div class="col-md-3">
                 <h4 class="Footer-sectionHeader">Resources</h4>
+                <a href="/contact" title="Contact The Rogg">Contact</a>
             </div>
             <div class="col-md-3">
                 <h4 class="Footer-sectionHeader">My Login</h4>
+                @if(Auth::guest())
+                    <form method="POST" action="/auth/login">
+
+                        {!! csrf_field() !!}
+
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" placeholder="email" required>
+                        </div>
+
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="password" name="password" value="{{ old('email') }}" placeholder="password" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label>
+                                <input type="checkbox" name="remember"> Remember Me
+                            </label>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary btn-block">Login</button>
+                    </form>
+                @endif
+
+                @if(Auth::check() || Auth::viaRemember())
+                    <a href="/auth/logout" title="Logout" class="btn btn-primary btn-block">Logout</a>
+                @endif
             </div>
             <div class="col-md-3">
                 <h4 class="Footer-sectionHeader">Follow the Rogg</h4>
