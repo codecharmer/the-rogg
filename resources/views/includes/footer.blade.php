744e6f7eac8740d01c8d2b081373680465a48205
Christopher Lamm <chris@theantichris.com>
2015-08-23 08:17:37 -0400

    Social media links.

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,22 +1,22 @@
 <footer class="Footer">
-    {{--<div class="container">--}}
-        {{--<div class="row">--}}
-            {{--<div class="col-md-3">--}}
-                {{--<h4 class="Footer-sectionHeader">About Rogg</h4>--}}
-            {{--</div>--}}
-            {{--<div class="col-md-3">--}}
-                {{--<h4 class="Footer-sectionHeader">Resources</h4>--}}
-            {{--</div>--}}
-            {{--<div class="col-md-3">--}}
-                {{--<h4 class="Footer-sectionHeader">My Login</h4>--}}
-            {{--</div>--}}
-            {{--<div class="col-md-3">--}}
-                {{--<h4 class="Footer-sectionHeader">Follow Rogg</h4>--}}
-                {{--<i class="fa fa-facebook-official"></i>--}}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <h4 class="Footer-sectionHeader">About The Rogg</h4>
+            </div>
+            <div class="col-md-3">
+                <h4 class="Footer-sectionHeader">Resources</h4>
+            </div>
+            <div class="col-md-3">
+                <h4 class="Footer-sectionHeader">My Login</h4>
+            </div>
+            <div class="col-md-3">
+                <h4 class="Footer-sectionHeader">Follow the Rogg</h4>
+                <a href="https://www.facebook.com/pages/The-Rogg/959813384063382" title="Follow The Rogg on Facebook"><i class="fa fa-facebook-official fa-2x"></i></a>
                 {{--<i class="fa fa-google-plus-square"></i>--}}
-                {{--<i class="fa fa-twitter-square"></i>--}}
+                <a href="https://twitter.com/therogg76" title="Follow The Rogg on Twitter"><i class="fa fa-twitter-square fa-2x"></i></a>
                 {{--<i class="fa fa-linkedin-square"></i>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
+            </div>
+        </div>
+    </div>
 </footer>
