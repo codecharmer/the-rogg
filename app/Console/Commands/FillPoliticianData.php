cd2dcd75a123dd69f9e3047d5b5669387a26f414
Christopher Lamm <chris@theantichris.com>
2015-08-29 10:34:48 -0400

    Start of console command to populate politician data.

---
+++ app/Console/Commands/FillPoliticianData.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace TheRogg\Console\Commands;
+
+use Illuminate\Console\Command;
+use TheRogg\Repositories\Politicians\PoliticianRepositoryInterface;
+
+class FillPoliticianData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'data:fillPoliticians';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fills politician data from the GovTrack.us bulk raw data service.';
+
+    /**
+     * Repository for the Politician domain model.
+     *
+     * @var PoliticianRepositoryInterface
+     */
+    private $politicianRepo;
+
+    /**
+     * Create a new command instance.
+     *
+     * @param PoliticianRepositoryInterface $politicianRepo
+     */
+    public function __construct(PoliticianRepositoryInterface $politicianRepo)
+    {
+        parent::__construct();
+
+        $this->politicianRepo = $politicianRepo;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+    }
+}
