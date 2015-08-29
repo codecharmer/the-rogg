9093ee40b700c4d78de2315ec5d330a393a50dfd
Christopher Lamm <chris@theantichris.com>
2015-08-29 09:31:09 -0400

    New sidebar.

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,4 +1,5 @@
-@if($review)
-    <h4>Random Review</h4>
-    @include('components.reviewSidebar')
-@endif
+<img class="Sidebar--logo" src="/assets/images/election2016.png" title="Election 2016 on The Rogg">
+
+<img class="Sidebar--logo" src="/assets/images/logo-white-background.png" title="The Rogg- Redress of Grievances">
+
+<p class="Sidebar--tagline">Redress of Grievances</p>
