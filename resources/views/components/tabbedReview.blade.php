da1cf60fe4c77d26eb7510589e569123757322a1
Christopher Lamm <chris@theantichris.com>
2015-08-28 20:57:41 -0400

    Initial styling and pulling in reviews.

---
+++ resources/views/components/tabbedReview.blade.php
@@ -0,0 +1,33 @@
+<div class="row Review">
+    <div class="col-md-3">
+        @if($review->user->photo)
+            <img class="ProfilePhoto" src="/assets/images/user-photos/{{ $review->user->photo }}">
+        @else
+            <img class="ProfilePhoto" src="/assets/images/generic-profile.png">
+        @endif
+    </div>
+    <div class="col-md-9">
+        <div class="Review--names">
+            <span>
+                {{ $review->user->username }}
+                @if(isset($review->politician))
+                    wrote a review for
+                    <a href="/politicians/{{ $review->politician->slug }}">{{ $review->politician->name }}</a>
+                @endif
+            </span>
+        </div>
+        <div class="Review--rating">
+            <span class="Review--bells" title="{{ $review->rating }}">
+                @for($i = 0; $i < $review->rating; $i++)
+                    <img class="Review--bell" src="/assets/images/liberty-bell.png">
+                @endfor
+
+                @for($i = 0; $i < 5 - $review->rating; $i++)
+                    <img class="Review--bell" src="/assets/images/liberty-bell-dark.png">
+                @endfor
+            </span>
+            <span class="Review--date"> {{ $review->timestamp->diffForHumans() }}</span>
+        </div>
+        <div class="Review--comment">{{ $review->comment }}</div>
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,53 +10,123 @@
                 </div>
             @endif
 
-            <div class="row">
+            <div class="row TabbedReviews">
                 <div class="col-md-12">
                     <div class="tabbable">
                         <ul class="nav nav-tabs nav-stacked col-md-3">
-                            <li><a href="#first" data-toggle="tab">1st Amendment</a></li>
-                            <li><a href="#second" data-toggle="tab">2nd Amendment</a></li>
-                            <li><a href="#third" data-toggle="tab">3rd Amendment</a></li>
-                            <li><a href="#fourth" data-toggle="tab">4th Amendment</a></li>
-                            <li><a href="#fifth" data-toggle="tab">5th Amendment</a></li>
-                            <li><a href="#sixth" data-toggle="tab">6th Amendment</a></li>
-                            <li><a href="#seventh" data-toggle="tab">7th Amendment</a></li>
-                            <li><a href="#eighth" data-toggle="tab">8th Amendment</a></li>
-                            <li><a href="#ninth" data-toggle="tab">9th Amendment</a></li>
-                            <li><a href="#tenth" data-toggle="tab">10th Amendment</a></li>
+                            <li class="TabbedReviews--linkContainer active">
+                                <a href="#first" data-toggle="tab">1st Amendment</a></li>
+                            <li class="TabbedReviews--linkContainer">
+                                <a href="#second" data-toggle="tab">2nd Amendment</a></li>
+                            <li class="TabbedReviews--linkContainer">
+                                <a href="#third" data-toggle="tab">3rd Amendment</a></li>
+                            <li class="TabbedReviews--linkContainer">
+                                <a href="#fourth" data-toggle="tab">4th Amendment</a></li>
+                            <li class="TabbedReviews--linkContainer">
+                                <a href="#fifth" data-toggle="tab">5th Amendment</a></li>
+                            <li class="TabbedReviews--linkContainer">
+                                <a href="#sixth" data-toggle="tab">6th Amendment</a></li>
+                            <li class="TabbedReviews--linkContainer">
+                                <a href="#seventh" data-toggle="tab">7th Amendment</a></li>
+                            <li class="TabbedReviews--linkContainer">
+                                <a href="#eighth" data-toggle="tab">8th Amendment</a></li>
+                            <li class="TabbedReviews--linkContainer">
+                                <a href="#ninth" data-toggle="tab">9th Amendment</a></li>
+                            <li class="TabbedReviews--linkContainer">
+                                <a href="#tenth" data-toggle="tab">10th Amendment</a></li>
                         </ul>
-                    </div>
 
-                    <div class="tab-content">
-                        <div class="tab-pane active" id="first">
-                            First
-                        </div>
-                        <div class="tab-pane" id="second">
-                            Second
-                        </div>
-                        <div class="tab-pane" id="third">
-                            Third
-                        </div>
-                        <div class="tab-pane" id="fourth">
-                            Fourth
-                        </div>
-                        <div class="tab-pane" id="fifth">
-                            Fifth
-                        </div>
-                        <div class="tab-pane" id="sixth">
-                            Sixth
-                        </div>
-                        <div class="tab-pane" id="seventh">
-                            Seventh
-                        </div>
-                        <div class="tab-pane" id="eighth">
-                            Eighth
-                        </div>
-                        <div class="tab-pane" id="ninth">
-                            Ninth
-                        </div>
-                        <div class="tab-pane" id="tenth">
-                            Tenth
+                        <div class="tab-content col-md-9">
+                            <div class="tab-pane active" id="first">
+                                <ul class="ReviewList">
+                                    @foreach($topReviews[0] as $review)
+                                        <li>
+                                            @include('components.tabbedReview')
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <div class="tab-pane" id="second">
+                                <ul class="ReviewList">
+                                    @foreach($topReviews[1] as $review)
+                                        <li>
+                                            @include('components.tabbedReview')
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <div class="tab-pane" id="third">
+                                <ul class="ReviewList">
+                                    @foreach($topReviews[2] as $review)
+                                        <li>
+                                            @include('components.tabbedReview')
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <div class="tab-pane" id="fourth">
+                                <ul class="ReviewList">
+                                    @foreach($topReviews[3] as $review)
+                                        <li>
+                                            @include('components.tabbedReview')
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <div class="tab-pane" id="fifth">
+                                <ul class="ReviewList">
+                                    @foreach($topReviews[4] as $review)
+                                        <li>
+                                            @include('components.tabbedReview')
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <div class="tab-pane" id="sixth">
+                                <ul class="ReviewList">
+                                    @foreach($topReviews[5] as $review)
+                                        <li>
+                                            @include('components.tabbedReview')
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <div class="tab-pane" id="seventh">
+                                <ul class="ReviewList">
+                                    @foreach($topReviews[6] as $review)
+                                        <li>
+                                            @include('components.tabbedReview')
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <div class="tab-pane" id="eighth">
+                                <ul class="ReviewList">
+                                    @foreach($topReviews[7] as $review)
+                                        <li>
+                                            @include('components.tabbedReview')
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <div class="tab-pane" id="ninth">
+                                <ul class="ReviewList">
+                                    @foreach($topReviews[8] as $review)
+                                        <li>
+                                            @include('components.tabbedReview')
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <div class="tab-pane" id="tenth">
+                                <ul class="ReviewList">
+                                    @foreach($topReviews[9] as $review)
+                                        <li>
+                                            @include('components.tabbedReview')
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
