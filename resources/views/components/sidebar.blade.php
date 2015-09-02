3a293326f17c3d6cee5975e87704a0b62895ec74
Christopher Lamm <chris@theantichris.com>
2015-09-02 07:43:47 -0400

    Updated sidebar to use just the Vote Now image.

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,8 +1,1 @@
-@if(Request::path() == 'purpose')
-    <img class="Sidebar--logo" src="/assets/images/voteNow.png" title="Vote Now!">
-@else
-    <img class="Sidebar--logo" src="/assets/images/election2016.png" title="Election 2016 on The Rogg">
-    <img class="Sidebar--logo" src="/assets/images/logo-white-background.png" title="The Rogg- Redress of Grievances">
-
-    <p class="Sidebar--tagline">Redress of Grievances</p>
-@endif
+<img class="Sidebar--logo" src="/assets/images/voteNow.png" title="Vote Now!">
